cba74ebc0190e84f9978a9e9204c183638f4c698
Lilian Chardon <55657944+Tequhella@users.noreply.github.com>
2021-05-09 19:47:10 +0200

    Add files via upload

---
+++ headers/gestionMap.h
@@ -0,0 +1,60 @@
+/*************************************************************/
+/* Kurīpāwārudo (inspiré du jeu Creeper World 2)             */
+/*-----------------------------------------------------------*/
+/* Module            : gestionMap.h                          */
+/* Numéro de version : 0.6                                   */
+/* Date              : 21/03/2021                            */
+/* Auteurs           : Lilian CHARDON                        */
+/*************************************************************/
+
+#ifndef _GESTIONMAP_H_INCLUDED_
+#define _GESTIONMAP_H_INCLUDED_
+
+#include "../headers/type.h"
+
+//----------------------------Gestion Map----------------------------//
+
+void remplirHasard (Map* m);
+
+void creerCaverne (Map* m, unsigned int pos, int randMoins);
+
+int testAccessibilitee (Map* m, int x, int y);
+
+void decache (Map* m);
+
+void remplirStock (Map* m);
+
+//---------------------------Gestion Block---------------------------//
+
+void casserBlock
+(
+    Map* m,
+    Coord* _bPos,
+    int* _blockAcasser,
+    int* _compte,
+    int* _nb_besoin
+);
+
+//--------------------------Gestion Entitee--------------------------//
+
+void ajouterEntitee (Case* c, int x, int y, unsigned int type, unsigned int* erreur);
+
+void constructionEntitee
+(
+    Map* m,
+    Coord* _ePos,
+    Coord* _tabEntitee,
+    int* _entiteeAcreer,
+    int* _compteE,
+    int* _nb_Entitee,
+    int* _nb_besoin,
+    int* _sortie
+);
+
+void remplirEnergieEntitee (Map* m, Coord* _tabEntitee, int* _nb_Entitee, int* _nb_besoin);
+
+void viderEnergieEntitee (Map* m, Coord* _tabEntitee, int* _nb_Entitee);
+
+void ajouterSpawnerHasard (Case* c, unsigned int nb_Spawner);
+
+#endif
---
+++ headers/gestionMem.h
@@ -0,0 +1,39 @@
+/*************************************************************/
+/* Kurīpāwārudo (inspiré du jeu Creeper World 2)             */
+/*-----------------------------------------------------------*/
+/* Module            : gestionMem.h                          */
+/* Numéro de version : 0.6                                   */
+/* Date              : 21/03/2021                            */
+/* Auteurs           : Lilian CHARDON                        */
+/*************************************************************/
+
+#ifndef _GESTIONMEM_H_INCLUDED_
+#define _GESTIONMEM_H_INCLUDED_
+
+#include "../headers/type.h"
+
+//-----------------------------Gestion Map-----------------------------//
+
+Map* creerCarte (double dimX, double dimY, char* nomDeLaCarte);
+
+void detruireCarte (Map* m);
+
+//----------------------------Gestion Block----------------------------//
+
+Block* creerBlock (unsigned int type, unsigned int typeStone, double posX, double posY);
+
+void detruireBlock (Block* b);
+
+//----------------------------Gestion Entité---------------------------//
+
+Entity* creerEntitee (double x, double y, unsigned int type);
+
+void detruireEntitee (Entity* e);
+
+
+
+
+
+
+
+#endif
